6ce43b6063bfba4ee1bd9db896c3ab9b5d48378e
happikin <yashesviraina@outlook.com>
2022-06-30 13:46:05 +0530

    list tests updated

--- tests/list-test.cpp
+++ tests/list-test.cpp
@@ -1,21 +1,18 @@
 #include "gtest/gtest.h"
 #include "list.hpp"
 
-TEST(listTest, test1) {
-    //arrange
-    //act
-    //assert
+TEST(listTest, length_test) {
     List<int> l{1,2,3,4};
     EXPECT_EQ (l.length(),4);
 }
 
-TEST(listTest, test2) {
+TEST(listTest, iterator_test) {
     List<int> list{1,2,3,4,5};
     List<int>::Iterator it = list.begin();
     EXPECT_EQ((*it), list.at(0));
 }
 
-TEST(listTest, test3) {
+TEST(listTest, overloaded_addition_test1) {
 
     List<int> list1{2,1,44,5,768,45};
     List<int> list2{23,66,41,656};
@@ -26,14 +23,14 @@ TEST(listTest, test3) {
     EXPECT_EQ(list3.length(), list1.length() + list2.length());
 }
 
-TEST(listTest, test4) {
+TEST(listTest, range_loop_test) {
     List<std::string> names{"Yashesvi","Arshi","Ajay","Chitralekha"};
-    // for(auto& item:names) {
-    //     std::cout << item << std::endl;
-    // }
+    unsigned int i{0};
+    for(auto& item:names)
+        EXPECT_EQ(item, names.at(i++));
 }
 
-TEST(listTest, test5) {
+TEST(listTest, overloaded_addition_test2) {
     List<int> list1,list2;
     for(int i=0;i<10000;i++) {
         list1.append(i*2);
@@ -45,7 +42,7 @@ TEST(listTest, test5) {
     EXPECT_EQ(list3.length(), list1.length() + list2.length());
 }
 
-TEST(listTest,test6) {
+TEST(listTest,sort_test) {
     List<int> list{1,3,2,6,4,34,65,65,7,4,65,87,98,78,67,58,64,63,6,63,63,2552,53,25545,43252,5523,52,52,2555,45,45355,57357,34,5,3,34,4353,45345,3544,3545,64545,645,456,45645,645,645,6,46,45,6456,45,4,47,3735,8282,4,4455,645,6456,45,645,656,756,65,88,678,7623,432,4324,324,325,45,34,534,34,645,645,645,76,457,76,456,3,452,43,24,31,42,342,3,325,346,45,76,765,6556,8};
     list.sort();
     for(auto i{0};i<list.length()-1;i++) {
@@ -56,7 +53,7 @@ TEST(listTest,test6) {
     // }
 }
 
-TEST(listTest, test7) {
+TEST(listTest, extractfor_test) {
     List<int> list{1,2,3,4,5,6,7};
     //std::cout << list.count([&](int& dataItem){ return dataItem%2==0;}) << std::endl;
     List<int> outList;
@@ -65,28 +62,23 @@ TEST(listTest, test7) {
     //     std::cout << item << std::endl;
 
     List<int> outList2 = list.extractFor([&](int& dataItem){ return dataItem<=5;},100);
-    for(auto& item:outList2) std::cout << item << std::endl;
+    //for(auto& item:outList2) std::cout << item << std::endl;
 }
 
-TEST(listTest, test8) {
+TEST(listTest, at_test) {
     List<int> list{1,2,3,4,5};
     list.at(1) = 22;
-    for(auto& item:list)
-        std::cout << item << std::endl;
+    EXPECT_NE(list.at(1), 2);
 }
 
-TEST(listTest, test9) {
-    List<int> list{1,2,3,4,5};
-    list.sort(true);
-    for(auto& item: list)
-        std::cout << item << std::endl;
-}
-
-TEST(randomTest, test1) {
+TEST(listTest, pop_front_test) {
     List<int> list;
-    list.insert(1,0);
-    std::cout << "Size: " << list.length() << std::endl;
-    auto e = list.popFront();
-    std::cout << "Size: " << list.length() << std::endl;
-    std::cout << "Popped ele: " << e;
+    int insertedValue = 1301;
+    list.insert(insertedValue,0);
+    uint oldSize = list.length();
+    auto poppedValue = list.popFront();
+    uint newSize = list.length();
+
+    EXPECT_EQ(oldSize, newSize+1);
+    EXPECT_EQ(insertedValue, poppedValue);
 }
