845aa7a34718be6979798b409e5902d80844e240
happikin <yashesviraina@outlook.com>
2022-07-01 17:32:08 +0530

    minor refactoring

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "matrix.hpp"
-using namespace ylib::math;
 
 int main(int argc, char *argv[]) {
     // matrix<int> mat1(2,2,2029);
-    matrix<int> mat2 = std::move(matrix<int>(2,2,23));
+    ycontainer::matrix<int> mat2 = std::move(ycontainer::matrix<int>(2,2,23));
 
-    matrix<int> mat3(1000,1000,10);
+    ycontainer::matrix<int> mat3(1000,1000,10);
     mat3.resize(1920,1080,true);
     return 0;
 }
