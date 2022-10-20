d309ca02cc54f0b872629ab377581a3d0dca1fda
happikin <yashesviraina@outlook.com>
2022-10-20 18:19:05 +0530

    added binary_tree::delete_node() functionality

--- binarytree/binary_tree.hpp
+++ binarytree/binary_tree.hpp
@@ -3,9 +3,22 @@
  * which will have n number of direct children possible
 */
 
+#ifdef DEBUG_MODE
+#endif
+    #include <iostream>
+#define isnull_macro(item) (item == nullptr)
+#define notnull_macro(item) (item != nullptr)
 namespace ycontainer {
     
     using size_t = unsigned long;
+    
+    enum insertion_type_e {
+        default_order = 0,
+        inorder,
+        preorder,
+        postorder
+    };
+
     template <typename type>
     class binary_tree {
         public:
@@ -15,30 +28,30 @@ namespace ycontainer {
                 type data;
                 node* right_node;
             };
-            enum insertion_type_e {
-                default_order = 0,
-                inorder,
-                preorder,
-                postorder
-            };
 
             binary_tree(insertion_type_e _insertiontype = insertion_type_e::default_order)
-            :   root(nullptr), height(0),
-                node_count(0), m_insertion_type(_insertiontype) {
+            :   m_root(nullptr), m_height(0),
+                m_node_count(0), m_insertion_type(_insertiontype) {
                 
             }
 
             void insert(type);
             type* flatten();
             node* find(type);
-        private:
-            node* root;
-            size_t height;
-            size_t node_count;
+            void delete_node(type _dataitem);
+            ~binary_tree();
+
+        private: /* data fields */
+            node* m_root;
+            size_t m_height;
+            size_t m_node_count;
             insertion_type_e m_insertion_type;
 
-        private:
+        private: /* internal member functions */
             void default_insert(type);
+            void inorder_insert(type);
+            void preorder_insert(type);
+            void postorder_insert(type);
 
     };
 
@@ -49,9 +62,17 @@ namespace ycontainer {
                 default_insert(_dataitem);
             } break;
 
-            case insertion_type_e::inorder: {} break;
-            case insertion_type_e::preorder: {} break;
-            case insertion_type_e::postorder: {} break;
+            case insertion_type_e::inorder: {
+                std::cout << "inorder to be implemented\n";
+            } break;
+            case insertion_type_e::preorder: {
+                std::cout << "preorder to be implemented\n";
+
+            } break;
+            case insertion_type_e::postorder: {
+                std::cout << "postorder to be implemented\n";
+
+            } break;
 
             default: {}
         }
@@ -59,19 +80,19 @@ namespace ycontainer {
 
     template <typename type>
     void binary_tree<type>::default_insert(type _dataitem) {
-        if(root == nullptr) {
+        if(m_root == nullptr) {
             node *tmp = new node;
             tmp->data = _dataitem;
             tmp->left_node = nullptr;
             tmp->right_node = nullptr;
-            root = tmp;
+            m_root = tmp;
             tmp = nullptr;
         } else {
             node *tmp = new node;
             tmp->data = _dataitem;
             tmp->left_node = nullptr;
             tmp->right_node = nullptr;
-            node *node_ptr = root;
+            node *node_ptr = m_root;
             while(true) {
                 if(_dataitem >= node_ptr->data) {
                     if(node_ptr->right_node == nullptr) {
@@ -96,10 +117,10 @@ namespace ycontainer {
 
     template <typename type>
     typename binary_tree<type>::node* binary_tree<type>::find(type _dataitem) {
-        if(root == nullptr) {
-            return root;
+        if(m_root == nullptr) {
+            return m_root;
         } else {
-            node* node_ptr = root;
+            node* node_ptr = m_root;
             while(true) {
                 if(_dataitem == node_ptr->data) {
                     return node_ptr;
@@ -116,4 +137,110 @@ namespace ycontainer {
         }
     }
 
+    template <typename type>
+    void binary_tree<type>::delete_node(type _dataitem) {
+        if(m_root != nullptr) {
+            node* parent_node_ptr = nullptr;
+            node* node_ptr = m_root;
+            while(true) {
+                if(_dataitem == node_ptr->data) {
+                    /**
+                     * @brief here we have few cases
+                     * 1. when we are deleting a leaf node
+                     * 2. deleting a node with just one child
+                     * 3. deleting a node with both childs
+                     * 4. deleting a node with both childs as subtrees
+                    */
+                    if(isnull_macro(node_ptr->left_node) && isnull_macro(node_ptr->right_node)) {
+                        std::cout << "first case\n";
+                        if(isnull_macro(parent_node_ptr)) {
+                            delete node_ptr;
+                            break;
+                        } else {
+                            if(parent_node_ptr->right_node == node_ptr) {
+                                parent_node_ptr->right_node = nullptr;
+                            } else {
+                                parent_node_ptr->left_node = nullptr;
+                            }
+                            delete node_ptr;
+                            break;
+                        }
+                    } else if(notnull_macro(node_ptr->left_node) ^ notnull_macro(node_ptr->right_node)) {
+                        std::cout << "second case\n";
+                        if(isnull_macro(parent_node_ptr)) {
+                            if(notnull_macro(node_ptr->left_node)) {
+                                m_root = node_ptr->left_node;
+                            } else m_root = node_ptr->right_node;
+                            delete node_ptr;
+                            break;
+                        } else {
+                            if(notnull_macro(node_ptr->left_node)) {
+                                if(parent_node_ptr->left_node == node_ptr) {
+                                    parent_node_ptr->left_node = node_ptr->left_node;
+                                } else parent_node_ptr->right_node = node_ptr->left_node;
+                            } else {
+                                if(parent_node_ptr->left_node == node_ptr) {
+                                    parent_node_ptr->left_node = node_ptr->right_node;
+                                } else parent_node_ptr->right_node = node_ptr->right_node;
+                            }
+                            delete node_ptr;
+                            break;
+                        }
+                    } else if(notnull_macro(node_ptr->left_node) && notnull_macro(node_ptr->right_node)) {
+                        std::cout << "third case\n";
+                        if(isnull_macro(parent_node_ptr)) {
+                            m_root = node_ptr->left_node;
+                            node* tmp_node_ptr = m_root;
+                            while(notnull_macro(tmp_node_ptr->right_node)) {
+                                tmp_node_ptr = tmp_node_ptr->right_node;
+                            }
+                            tmp_node_ptr->right_node = node_ptr->right_node;
+                            delete node_ptr;
+                            break;
+                        } else {
+                            node* tmp_node_ptr = nullptr;
+                            if(parent_node_ptr->left_node == node_ptr) {
+                                parent_node_ptr->left_node = node_ptr->left_node;
+                                tmp_node_ptr = parent_node_ptr->left_node;
+                            } else {
+                                parent_node_ptr->right_node = node_ptr->left_node;
+                                tmp_node_ptr = parent_node_ptr->right_node;
+                            }
+                            while(notnull_macro(tmp_node_ptr->right_node)) {
+                                tmp_node_ptr = tmp_node_ptr->right_node;
+                            }
+                            tmp_node_ptr->right_node = node_ptr->right_node;
+                            delete node_ptr;
+                            break;
+                        }
+                    }
+                } else if(_dataitem < node_ptr->data) {
+                    if(node_ptr->left_node != nullptr) {
+                        parent_node_ptr = node_ptr;
+                        node_ptr = node_ptr->left_node;
+                    } else break;
+                } else if(_dataitem > node_ptr->data) {
+                    if(node_ptr->right_node != nullptr) {
+                        parent_node_ptr = node_ptr;
+                        node_ptr = node_ptr->right_node;
+                    } else break;
+                }
+            }
+        }
+    }
+
+    template <typename type>
+    binary_tree<type>::~binary_tree() {
+        if(notnull_macro(m_root->left_node)) {
+            std::thread([&](){
+                
+            });
+        }
+        if(notnull_macro(m_root->right_node)) {
+            std::thread([&](){
+                
+            });
+        }
+        std::cout << "destruction done\n";
+    }
 } // namespace ycontainer
--- binarytree/main.cpp
+++ binarytree/main.cpp
@@ -2,24 +2,35 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-    ycontainer::binary_tree<int> b;
-    b.insert(10);
-    b.insert(12);
-    b.insert(12);
-    b.insert(7);
-    b.insert(9);
+
+    ycontainer::binary_tree<int> b(
+        ycontainer::insertion_type_e::default_order);
+    
+    b.insert(100);
+    b.insert(150);
+    b.insert(125);
+    b.insert(120);
+    b.insert(130);
+    b.insert(175);
+    b.insert(155);
+    b.insert(180);
+
+    b.insert(50);
+    b.insert(75);
+    b.insert(85);
+    b.insert(70);
+    b.insert(25);
+    b.insert(35);
+    b.insert(15);
 
     ycontainer::binary_tree<int>::node* tree_node_ptr = nullptr;
-    if((tree_node_ptr = b.find(10)) == nullptr) {
+    if((tree_node_ptr = b.find(100)) == nullptr) {
         std::cout << "could not find\n";
     } else {
         std::cout << "found\n";
-        tree_node_ptr->data = 100;
     }
 
-    if(b.find(10) == nullptr) {
-        std::cout << "could not find\n";
-    } else std::cout << "found\n";
+    b.delete_node(100);
 
     return 0;
 }
