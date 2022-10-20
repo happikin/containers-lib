17070b3aa84f0fea813c3ac4114119c8dd854881
happikin <yashesviraina@outlook.com>
2022-10-20 13:57:12 +0530

    added insertion_type_e enum to allow bst, inorder, preorder & postorder insertion methods

--- binarytree/binary_tree.hpp
+++ binarytree/binary_tree.hpp
@@ -9,23 +9,56 @@ namespace ycontainer {
     template <typename type>
     class binary_tree {
         public:
+
             struct node {
                 node* left_node;
                 type data;
                 node* right_node;
             };
-            binary_tree() : root(nullptr), height(0),  node_count(0) {}
+            enum insertion_type_e {
+                default_order = 0,
+                inorder,
+                preorder,
+                postorder
+            };
+
+            binary_tree(insertion_type_e _insertiontype = insertion_type_e::default_order)
+            :   root(nullptr), height(0),
+                node_count(0), m_insertion_type(_insertiontype) {
+                
+            }
+
             void insert(type);
             type* flatten();
+            node* find(type);
         private:
             node* root;
             size_t height;
             size_t node_count;
+            insertion_type_e m_insertion_type;
+
+        private:
+            void default_insert(type);
 
     };
 
     template <typename type>
     void binary_tree<type>::insert(type _dataitem) {
+        switch(m_insertion_type) {
+            case insertion_type_e::default_order: {
+                default_insert(_dataitem);
+            } break;
+
+            case insertion_type_e::inorder: {} break;
+            case insertion_type_e::preorder: {} break;
+            case insertion_type_e::postorder: {} break;
+
+            default: {}
+        }
+    }
+
+    template <typename type>
+    void binary_tree<type>::default_insert(type _dataitem) {
         if(root == nullptr) {
             node *tmp = new node;
             tmp->data = _dataitem;
@@ -56,6 +89,28 @@ namespace ycontainer {
                     } else {
                         node_ptr = node_ptr->left_node;
                     }
+                }
+            }
+        }
+    }
+
+    template <typename type>
+    typename binary_tree<type>::node* binary_tree<type>::find(type _dataitem) {
+        if(root == nullptr) {
+            return root;
+        } else {
+            node* node_ptr = root;
+            while(true) {
+                if(_dataitem == node_ptr->data) {
+                    return node_ptr;
+                } else if(_dataitem >= node_ptr->data) {
+                    if(node_ptr->right_node != nullptr) {
+                        node_ptr = node_ptr->right_node;
+                    } else return node_ptr->right_node;
+                } else if(_dataitem < node_ptr->data) {
+                    if(node_ptr->left_node != nullptr) {
+                        node_ptr = node_ptr->left_node;
+                    } else return node_ptr->left_node;
                 }
             }
         }
--- binarytree/main.cpp
+++ binarytree/main.cpp
@@ -1,4 +1,5 @@
 #include "binary_tree.hpp"
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
     ycontainer::binary_tree<int> b;
@@ -7,6 +8,18 @@ int main(int argc, char const *argv[]) {
     b.insert(12);
     b.insert(7);
     b.insert(9);
+
+    ycontainer::binary_tree<int>::node* tree_node_ptr = nullptr;
+    if((tree_node_ptr = b.find(10)) == nullptr) {
+        std::cout << "could not find\n";
+    } else {
+        std::cout << "found\n";
+        tree_node_ptr->data = 100;
+    }
+
+    if(b.find(10) == nullptr) {
+        std::cout << "could not find\n";
+    } else std::cout << "found\n";
 
     return 0;
 }
